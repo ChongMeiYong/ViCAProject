785cc9c6da7f37de6bbe4d26825abbf34392fd57
ChongMeiYong <chongmei1997@gmail.com>
2019-12-15 01:24:10 +0800

    Commit

--- lib/course.dart
+++ lib/course.dart
@@ -1,4 +1,5 @@
 class Course {
+  Source source;
   String id,
       cname,
       owner,
@@ -7,10 +8,36 @@ class Course {
       image;
 
   Course(
-      {this.id,
+      {this.source,
+      this.id,
       this.cname,
       this.owner,
       this.descp,
       this.duration,
       this.image});
+
+  factory Course.fromJson(Map<String , dynamic> json) {
+    return Course(
+      source: Source.fromJson(json["source"]),
+      id: json["id"],
+      cname: json["cname"],
+      owner: json["owner"],
+      descp: json["descp"],
+      duration: json["duration"],
+      image: json["image"]); 
+    }
+  }
+
+  class Source {
+    String id;
+    String owner;
+
+  Source({this.id, this.owner});
+
+  factory Source.fromJson(Map<String, dynamic> json){
+    return Source(
+      id: json["id"] as String,
+      owner: json["owner"] as String,
+    );
+  }
 }
